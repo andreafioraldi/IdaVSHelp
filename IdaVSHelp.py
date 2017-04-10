88228f30bbee298900d343cf414c79644a10f274
Andrea Fioraldi <andreafioraldi@gmail.com>
2017-04-10 23:29:24 +0200

    First script version

---
+++ IdaVSHelp.py
@@ -0,0 +1,61 @@
+__author__ = "Andrea Fioraldi"
+__copyright__ = "Copyright 2017, Andrea Fioraldi"
+__license__ = "MIT"
+__email__ = "andreafioraldi@gmail.com"
+
+import idaapi
+import subprocess
+import os
+import idc
+
+def query():
+    func = idaapi.get_highlighted_identifier()
+    args = [idc.GetIdaDirectory() + '\\plugins\\Microsoft Help Viewer\\v2.2\\HlpViewer.exe',
+        '/catalogName',
+        'VisualStudio14',
+        '/helpQuery',
+        'method=f1&query=' + func,
+        '/locale',
+        'en-US',
+        '/launchingApp',
+        'Microsoft,VisualStudio,14.0'
+        ]
+    subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+def open_help():
+    args = [idc.GetIdaDirectory() + '\\plugins\\Microsoft Help Viewer\\v2.2\\HlpViewer.exe',
+        '/catalogName',
+        'VisualStudio14',
+        '/locale',
+        'en-US',
+        '/launchingApp',
+        'Microsoft,VisualStudio,14.0'
+        ]
+    subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+
+MENU_PATH = 'Edit/Other'
+class IdaVSHelp_plugin_t(idaapi.plugin_t):
+    flags = idaapi.PLUGIN_KEEP
+    comment = ""
+
+    help = "IdaVSHelp: Visual Studio Help Referene for IDA"
+    wanted_name = "IDA Visual Studio Help"
+    wanted_hotkey = "Alt-9"
+
+    def init(self):
+        r = idaapi.add_menu_item(MENU_PATH, 'Open Microsoft Help Viewer', '', 1, open_help, tuple())
+        if r is None:
+            idaapi.msg("IdaVSHelp: add menu failed!\n")
+        idaapi.msg("IdaVSHelp: initialized\n")
+        return idaapi.PLUGIN_KEEP
+
+    def run(self, arg):
+        query()
+
+    def term(self):
+        idaapi.msg("IdaVSHelp: terminated\n")
+
+def PLUGIN_ENTRY():
+    return IdaVSHelp_plugin_t()
+
